7a23f2cfbdc7b77523f9961a77f73ced1841e279
Udi Lipshitsz <udilipsh@gmail.com>
2025-10-20 17:38:29 +0300

    הוספתי שתי מתודות עם מספרי זהות של שני השותפים. שיניתי את שם התכנית. והגדרנו את את המחלקה לpartial

---
+++ Stage0/Program3997.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace stage0;
+internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Welcome3997();
+        Welcome6339();
+
+    }
+    static partial void Welcome6339();
+    private static void Welcome3997()
+    {
+        Console.WriteLine("Hello, World!");
+        Console.Write("Enter your name: ");
+        string? n = Console.ReadLine();
+        Console.WriteLine("{0}, welcome to my first application!", n);
+    }
+}
