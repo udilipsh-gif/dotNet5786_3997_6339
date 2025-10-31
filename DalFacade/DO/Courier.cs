76907914a94edb9696820940848882c759993686
david davidovich <david48483@gmail.com>
2025-10-31 03:00:59 +0200

    הוספתי תיעוד

--- DalFacade/DO/Courier.cs
+++ DalFacade/DO/Courier.cs
@@ -7,6 +7,15 @@
 /// <remarks>This record encapsulates information about a courier, such as their contact details,  delivery
 /// capabilities, and working status. It is designed to be used in systems that manage  delivery personnel and their
 /// assignments.</remarks>
+/// <param name="Id"> The unique identifier for the courier.</param>
+/// <param name="Name"> The full name of the courier.</param>
+/// <param name="Phone"> The contact phone number of the courier.</param>
+/// <param name="Email"> The email address of the courier.</param>
+/// <param name="Password"> The password for the courier's account.</param>
+/// <param name="Active"> Indicates whether the courier is currently active and available for deliveries.</param>
+/// <param name="MaxDistanceDelivery"> The maximum distance the courier is willing to travel for deliveries.</param>
+/// <param name="TheTypeShipment"> The type of shipment the courier is equipped to handle (e.g., car, motorcycle, bike, foot).</param>
+/// <param name="WorkingSince"> The date and time when the courier started working.</param>
 public record Courier
 {
     public required int Id { get; init; }
@@ -17,5 +26,5 @@ public record Courier
     public required bool Active { get; set; }
     public double MaxDistanceDelivery { get; set; }
     public required TheTypeShipment TheTypeShipment { get; set; }
-    public DateTime WorkingSince { get; init; } = DateTime.Now;
+    public required DateTime WorkingSince { get; init; } = DateTime.Now;
 }
