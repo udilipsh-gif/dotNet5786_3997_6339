3953af02667156f661f0f1f86950b406ccf1c431
Udi Lipshitsz <udilipsh@gmail.com>
2025-10-31 03:23:42 +0200

    my config

---
+++ DalFacade/DO/Config.cs
@@ -0,0 +1,11 @@
+﻿namespace DO;
+
+internal class Config
+{
+
+    public required int NextOrderId { get; init; }
+    public required int NextDeliveryId { get; init; }
+    public required DateTime Clock { get; set; }
+
+    public required int me
+}
