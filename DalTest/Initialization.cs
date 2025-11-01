98f0eaff9dff0847061cb4aa1135f24bf6c01870
Udi Lipshitsz <udilipsh@gmail.com>
2025-11-01 23:00:20 +0200

    add Initialization

---
+++ DalTest/Initialization.cs
@@ -0,0 +1,27 @@
+﻿
+
+namespace DalTest;
+using DalApi;
+using DO;
+
+public static class Initialization
+{
+    private static ICourier? s_dalStudent; //stage 1
+    private static IOrder? s_dalCourse; //stage 1
+    private static IDelivery? s_dalLink; //stage 1
+    private static IConfig? s_dalConfig; //stage 1
+
+    private static readonly Random s_rand = new();
+
+    private static createCourier = () =>
+    {
+        return new Courier
+        {
+            Id = s_rand.Next(1, 1000),
+            Name = "Test Courier",
+            Vehicle = Vehicle.Bike,
+            IsAvailable = true
+        };
+    };
+
+}
