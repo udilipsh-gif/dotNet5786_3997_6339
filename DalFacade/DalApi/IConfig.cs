36ad37d9b2897a2d194c116ce9135587b04256ef
Udi Lipshitsz <udilipsh@gmail.com>
2025-11-01 22:09:19 +0200

    add file IConfig

---
+++ DalFacade/DalApi/IConfig.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace DalApi;
+
+
+public interface IConfig
+{
+    DateTime Clock { get; set; }
+    int ManagerId { get; set; }
+    int MaxRange { get; set; }
+    string PasswordManager { get; set; }
+    string storeAddress { get; set; }
+    double maxDeliveryRange { get; set; }
+    double AvgSpeedCar { get; set; }
+    double AvgSpeedMotorcycle { get; set; }
+    double AvgSpeedBike { get; set; }
+    double AvgSpeedFoot { get; set; }   
+    TimeSpan MaxDeliveryTime { get; set; }
+    TimeSpan RiskRange { get; set; }
+    TimeSpan MaxTimeInactivity { get; set; }
+    void Reset();
+}
