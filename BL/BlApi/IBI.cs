44a8540b3b108185f3989adbc11f0719a2e3f924
Udi Lipshitsz <udilipsh@gmail.com>
2025-11-22 23:52:37 +0200

    סיום פרק 8 טכנית

---
+++ BL/BlApi/IBI.cs
@@ -0,0 +1,17 @@
+﻿
+namespace BlApi;
+
+public interface IBI
+{
+    IAdmin Admin { get; }
+    ICourier Courier { get; }
+    IOrder Order { get; }
+    IClosedDeliveryInList ClosedDeliveryInList { get; }
+    ICourierInList CourierInList { get; }
+    IDelivery Delivery { get; }
+    IDeliveryPerOrderInList DeliveryPerOrderInList { get; }
+    IOpenOrderInList OpenOrderInList { get; }
+    IOrderInProgress OrderInProgress { get; }
+    IOrderInList OrderInList { get; }
+
+}
