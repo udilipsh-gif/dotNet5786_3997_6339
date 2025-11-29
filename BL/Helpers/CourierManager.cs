57a7eadc88a765be9347aaf2567087973f2c29cf
david davidovich <david48483@gmail.com>
2025-11-29 22:44:36 +0200

    הוצאה לקובץ TOOLS

--- BL/Helpers/CourierManager.cs
+++ BL/Helpers/CourierManager.cs
@@ -30,13 +30,13 @@ internal static class CourierManager
     }
     internal static void Create(BO.Courier boCourier)//יצירת שליח בדאטה בייס בסגנון ישות DO
     {
-        if (!IsValidId(boCourier.Id))
+        if (!Tools.IsValidId(boCourier.Id))
             throw new BO.InvalidIdException("Invalid ID.");
-        if (!IsValidPhone(boCourier.Phone))
+        if (!Tools.IsValidPhone(boCourier.Phone))
             throw new BO.InvalidPhoneException("Invalid phone number.");
-        if (!IsValidEmail(boCourier.Email))
+        if (!Tools.IsValidEmail(boCourier.Email))
             throw new BO.InvalidEmailException("Invalid email address.");
-        if (!IsStrongPassword(boCourier.Password))
+        if (!Tools.IsStrongPassword(boCourier.Password))
             throw new BO.WeakPasswordException("Password is not strong enough.");
 
 
@@ -245,64 +245,4 @@ internal static class CourierManager
 
         return deliveriesOnTime.Count();
     }
-
-
-    //בדיקות תקינות של ערכים
-    private static bool IsValidEmail(string email)
-    {
-        try
-        {
-            var addr = new System.Net.Mail.MailAddress(email);
-            return addr.Address == email;
-        }
-        catch
-        {
-            return false;
-        }
-    }
-    private static bool IsValidPhone(string phone)
-    {
-        return Regex.IsMatch(phone, @"^0\d{8,9}$") ||
-        Regex.IsMatch(phone, @"^\+?[1-9]\d{1,14}$");
-
-    }
-    private static bool IsStrongPassword(string password)
-    {
-        if (password.Length < 8)
-            return false;
-        bool hasUpper = false, hasLower = false, hasDigit = false, hasSpecial = false;
-        foreach (char c in password)
-        {
-            if (char.IsUpper(c)) hasUpper = true;
-            else if (char.IsLower(c)) hasLower = true;
-            else if (char.IsDigit(c)) hasDigit = true;
-            else hasSpecial = true;
-        }
-        return hasUpper && hasLower && hasDigit && hasSpecial;
-    }
-    private static bool IsValidId(int id)
-    {
-        int tempId = id;
-        int sum = 0;
-        tempId = tempId / 10;
-        for (int i = 1; i < 9; i++)
-        {
-            int temp = tempId % 10;
-            if (i % 2 == 0)
-            {
-                sum = sum + temp;
-            }
-            else
-            {
-                temp = temp * 2;
-                sum = sum + (temp % 10 + temp / 10);
-            }
-            tempId = tempId / 10;
-        }
-
-        return (id % 10 == (10 - (sum % 10)));
-    }
-
-
-
 }
