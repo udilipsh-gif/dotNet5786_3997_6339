0d94535ba8b2345c063f71b7d284dfb8fc5ad872
david davidovich <david48483@gmail.com>
2025-11-29 23:38:47 +0200

    המשך

--- BL/Helpers/CourierManager.cs
+++ BL/Helpers/CourierManager.cs
@@ -82,29 +82,20 @@ internal static class CourierManager
         };
         return boCourier;//מחזירים שליח מומר
     }
-    private static BO.OrderInProgress GetOrderInProgres(int courierId)
+    private static BO.OrderInProgress? GetOrderInProgres(int courierId)
     {
 
         // מקבל אוסף של כל המשלוחים של השליח שעדיין לא הסתיימו
         IEnumerable<DO.Delivery> allDeliveries = s_dal.Delivery.ReadAll(d =>
             d.CourierId == courierId &&
-            d.EndDelivery == null
+            d.EndDelivery == null &&
+            s_dal.Order.Read(d.OrderId)?.OrderStatus == DO.OrderStatus.DELIVERING
         );
 
-        // מקבל את המשלוח הפעיל הראשון שהזמנה שלו במצב DELIVERING
-        DO.Delivery? activeDelivery = allDeliveries.FirstOrDefault(d =>
-        {
-            DO.Order? order = s_dal.Order.Read(d.OrderId);
-            return order != null && order.OrderStatus == DO.OrderStatus.DELIVERING;
-        });
-
-
-        if (activeDelivery == null)
+        if (!allDeliveries.Any())
             return null;
 
-
-        return CreateOrderInProgress(activeDelivery);
-
+        return CreateOrderInProgress(allDeliveries.First());
     }
     private static BO.OrderInProgress CreateOrderInProgress(DO.Delivery delivery)
     {
@@ -258,7 +249,7 @@ internal static class CourierManager
     {
         IEnumerable<DO.Delivery> deliveriesOnTime = s_dal.Delivery.ReadAll(d =>
                d.CourierId == doCourier.Id &&
-               d.EndDelivery == EndDelivery.DELIVERED &&
+               d.EndDelivery == DO.EndDelivery.DELIVERED &&
                d.TimeEndDelivery != null &&
                d.TimeEndDelivery - d.OrderDate <= s_dal.Config.MaxDeliveryTime
         );
