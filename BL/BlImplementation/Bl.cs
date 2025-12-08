4e6caca3d09349ecdc4a61deb91eaf7296076f83
Udi Lipshitsz <udilipsh@gmail.com>
2025-12-08 19:24:52 +0200

    internal class Bl

---
+++ BL/BlImplementation/Bl.cs
@@ -0,0 +1,18 @@
+﻿namespace BlImplementation;
+using BlApi;
+internal class Bl : IBl
+{
+    public IAdmin Admin { get; } = new AdminImplementation();
+    public ICourier Courier { get; } = new CourierImplementation();
+    public IOrder Order { get; } = new OrderImplementation();
+
+    //public IClosedDeliveryInList ClosedDeliveryInList { get; } = new ClosedDeliveryInListImplementation();
+    //public ICourierInList CourierInList { get; } = new CourierInListImplementation();
+    //public IDelivery Delivery { get; } = new DeliveryImplementation();
+    //public IDeliveryPerOrderInList DeliveryPerOrderInList { get; } = new DeliveryPerOrderInListImplementation();
+    //public IOpenOrderInList OpenOrderInList { get; } = new OpenOrderInListImplementation();
+    //public IOrderInProgress OrderInProgress { get; } = new OrderInProgressImplementation();
+    //public IOrderInList OrderInList { get; } = new OrderInListImplementation();
+
+
+}
