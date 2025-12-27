1f5d53c38efaf47e3c72403a6b0e34f6ed4db3f0
david davidovich <david48483@gmail.com>
2025-12-28 01:05:30 +0200

    פרק 7

--- PL/Courier/CourierListWindow.xaml.cs
+++ PL/Courier/CourierListWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +20,35 @@ namespace PL.Courier
     /// </summary>
     public partial class CourierListWindow : Window
     {
+        static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
+
         public CourierListWindow()
         {
             InitializeComponent();
+
+            this.Loaded += MainWindow_Loaded;
+
+            this.Closed += MainWindow_Close;
+
+
         }
+
+        private void MainWindow_Close(object? sender, EventArgs e)
+        {
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+ 
+        }
+
+        public IEnumerable<BO.CourierInList> CourierList
+        {
+            get { return (IEnumerable<BO.CourierInList>)GetValue(CourierListProperty); }
+            set { SetValue(CourierListProperty, value); }
+        }
+
+        public static readonly DependencyProperty CourierListProperty =
+            DependencyProperty.Register("CourierInList", typeof(IEnumerable<BO.CourierInList>), typeof(CourierListWindow), new PropertyMetadata(null));
     }
 }
