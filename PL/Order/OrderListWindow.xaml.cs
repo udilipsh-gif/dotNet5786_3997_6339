a0242602561aa9691c8791de0c879d82df37d7ed
Udi Lipshitsz <udilipsh@gmail.com>
2026-01-05 00:35:36 +0200

    סיום מסכי הזמנות. הוספתי פונקציה לשליחת מייל בטולס של PL/ כמו כן שליחת מייל במקרה ביטול הזמנה בודדת.

--- PL/Order/OrderListWindow.xaml.cs
+++ PL/Order/OrderListWindow.xaml.cs
@@ -33,10 +33,21 @@ public partial class OrderListWindow : Window
 
     public BO.TypeOfOrder TypeOfOrderFilter { get; set; } = BO.TypeOfOrder.ALL;
 
+
+    //private Visibility _cancelButtonVisibility = Visibility.Visible;
+    //public Visibility CancelButtonVisibility
+    //{
+    //    get { return _cancelButtonVisibility; }
+    //    set
+    //    {
+    //        _cancelButtonVisibility = value;
+    //    }
+    //}
+
     private void LoadOrders()
     {
         // 1. שליפת כל הנתונים מה-BL ללא סינון ראשוני כלל
-        var allOrders = s_bl?.Order.ReadAll(CURRENT_MANAGER_ID, null, null, null);
+        var allOrders = s_bl.Order.ReadAll(CURRENT_MANAGER_ID, null, null, null);
 
         // 2. ביצוע סינון כפול בעזרת LINQ
         var filteredResults = allOrders.Where(order =>
@@ -45,7 +56,7 @@ public partial class OrderListWindow : Window
         );
 
         // 3. עדכון הרשימה המוצגת במסך
-         OrderList = new ObservableCollection<BO.OrderInList>( filteredResults);
+        OrderList = new ObservableCollection<BO.OrderInList>(filteredResults);
         //OrderList = new ObservableCollection<BO.OrderInList>(s_bl.Order.ReadAll(CURRENT_MANAGER_ID,null,null,null));
     }
 
@@ -54,17 +65,15 @@ public partial class OrderListWindow : Window
 
     private void Window_Loaded(object sender, RoutedEventArgs e)
     {
-        LoadOrders();
         s_bl.Order.AddObserver(orderListObserver);
+        orderListObserver();
     }
 
     private void Window_Closed(object sender, EventArgs e)
         => s_bl.Order.RemoveObserver(orderListObserver);
 
     private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-    {
-        LoadOrders();
-    }
+        => orderListObserver();
 
     private void Add_Edit_Order_Click(object sender, RoutedEventArgs e)
     {
@@ -79,6 +88,31 @@ public partial class OrderListWindow : Window
             orderWindow = new OrderWindow(0);
         }
         orderWindow.Show();
+    }
+
+    private void btnCancelOrder_Click(object sender, RoutedEventArgs e)
+    {
+        var result = MessageBox.Show(
+            "האם אתה רוצה לבטל את ההזמנה הזו?",
+            "הזמנה בוטלה",
+            MessageBoxButton.YesNo,
+            MessageBoxImage.Warning);
+
+        if (result != MessageBoxResult.Yes)
+            return;
+        if (sender is Button button
+            && button.DataContext is BO.OrderInList orderInList)
+        {
+            try
+            {
+                s_bl.Order.Cancel(CURRENT_MANAGER_ID, orderInList.OrderId);
+                MessageBox.Show($"הזמנה מס' {orderInList.OrderId} בוטלה בהצלחה");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error canceling the order: {ex.Message}");
+            }
+        }
     }
 
 }
