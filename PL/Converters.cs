2253c67dc8a5dff4fda4cf89399400d5ec5be09d
david davidovich <david48483@gmail.com>
2026-01-05 23:56:16 +0200

    כנל

--- PL/Converters.cs
+++ PL/Converters.cs
@@ -46,6 +46,21 @@ public class NullToVisibilityConverter : IValueConverter
     }
 }
 
+public class BoolToVisibilityConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is bool boolValue)
+            return boolValue ? Visibility.Visible : Visibility.Collapsed;
+        return Visibility.Collapsed;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
+
 public class NullToBooleanConverter : IValueConverter
 {
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
