06232b4760de15ab0a2e858320efc407492732a5
david davidovich <david48483@gmail.com>
2026-01-11 21:39:16 +0200

    תיקון מרחק

--- BL/Helpers/OrderManager.cs
+++ BL/Helpers/OrderManager.cs
@@ -95,6 +95,11 @@ internal static class OrderManager
 
         var adressCoordinates = Tools.GetGeocodingSync(boOrder.Addres);
 
+        var distense = Tools.GetDistance(adressCoordinates?.Lat ?? 0, adressCoordinates?.Lng ?? 0, s_dal.Config.Latitude ?? 0, s_dal.Config.Longitude ?? 0);
+
+        if (distense == 0 || distense > s_dal.Config.MaxDeliveryRange)
+            throw new BO.BlInvalidOperationException($"המרחק {distense} KM עולה על טווח השילוח {s_dal.Config.MaxDeliveryRange} KM");
+
         DO.Order doOrder = new DO.Order
         {
             Id = 0,
@@ -530,7 +535,7 @@ internal static class OrderManager
             ?? throw new BO.BlDoesNotExistException("Courier not found");
 
         var query = from doOrder in s_dal.Order.ReadAll(o => o.OrderStatus == DO.OrderStatus.OPEN)
-                    let distense = Tools.GetDistance(doOrder)
+                    let distense = Tools.GetActualDistance(doOrder.Addres, (BO.TheTypeShipment)doCourier.TypeShipment)
                     where ((filter == null || (BO.TypeOfOrder)doOrder.TypeOfOrder == filter) && (distense <= doCourier.MaxDistanceDelivery))
                     select new BO.OpenOrderInList
                     {
@@ -538,8 +543,8 @@ internal static class OrderManager
                         TypeOfOrder = (BO.TypeOfOrder)doOrder.TypeOfOrder,
                         Weight = doOrder.Weight,
                         Address = doOrder.Addres,
-                        ActualDistance = null,//לא מחושב עדיין
-                        DistanceKm = distense,
+                        DistanceKm = doOrder.DistanceKm ?? 0,
+                        ActualDistance = distense,
                         EstimatedDeliveryTime = null,
                         ScheduleStatus = Tools.GetScheduleStatus(doOrder),
                         TimeLeftForDelivery = Tools.GetTimeLeftForDelivery(doOrder),
